cccfe5411b4e52aecb0ff01823bafebe6b900617
Ivan yu <gun123kk@gmail.com>
2020-09-21 02:13:45 +0800

    (NTUT)(TRACKER) read from json add new items

--- read_vott_id_json.py
+++ read_vott_id_json.py
@@ -1,25 +1,35 @@
-import os, shutil
+import os
 import json
-import numpy as np
-from collections import defaultdict
 
 class read_vott_id_json():
 # private
     def __clear_all_parameter_value(self):
-        self.id = ""
+        self.asset_id = ""
+        self.asset_format = ""
+        self.asset_name = ""
         self.asset_path = ""
         self.video_size = [3840, 2160]
+
+        self.parent_id = ""
+        self.parent_name = ""
+
         self.timestamp = 0
         self.tags = ""
         self.regions_id = ""
         self.boundingBox = [0,0,0,0]
-        #self.points = [0,0,0,0]
 
     def __print_read_parameter_from_json(self):
-        print("id: %s" % self.id)
-        print("path: %s" % self.asset_path)
+        print("asset_id: %s" % self.asset_id)
+        print("asset_format: %s" % self.asset_format)
+        print("asset_name: %s" % self.asset_name)
+        print("asset_path: %s" % self.asset_path)
         print("width: %d" % self.video_size[0])
         print("height: %d" % self.video_size[1])
+
+        print("parent_id: %s" % self.parent_id)
+        print("parent_name: %s" % self.parent_name)
+        print("parent_path: %s" % self.parent_path)
+
         print("timestamp: %.5f" % self.timestamp)
         print("tags: %s" % self.tags)
         print("boundingBox height: %s" % self.boundingBox[0])
@@ -43,18 +53,24 @@ class read_vott_id_json():
             with open(self.file_path, 'r') as reader:
                 print("open_ok")
                 jf = json.loads(reader.read())
-                self.id = jf['asset']['id']
-                self.asset_path = jf['asset']['format']
+                self.asset_id = jf['asset']['id']
+                self.asset_format = jf['asset']['format']
+                self.asset_name = jf['asset']['name']
+                self.asset_path = jf['asset']['path']
                 self.video_size[0] = jf['asset']['size']['width']
                 self.video_size[1] = jf['asset']['size']['height']
                 self.timestamp = jf['asset']['timestamp']
+
+                self.parent_id = jf['asset']['parent']['id']
+                self.parent_name = jf['asset']['parent']['name']
+                self.parent_path = jf['asset']['parent']['path']
 
                 self.tags = jf['regions'][0]['tags'][0]
                 self.boundingBox[0] = jf['regions'][0]['boundingBox']["height"]
                 self.boundingBox[1] = jf['regions'][0]['boundingBox']["width"]
                 self.boundingBox[2] = jf['regions'][0]['boundingBox']["left"]
                 self.boundingBox[3] = jf['regions'][0]['boundingBox']["top"]
-
+                print("read form json ok")
                 self.__print_read_parameter_from_json()
                 
             return False
@@ -62,11 +78,26 @@ class read_vott_id_json():
             print(' wrong format: '+ self.file_path)
             return True
 
-    def get_id(self):
-        return self.id
+    def get_asset_id(self):
+        return self.asset_id
+    
+    def get_asset_format(self):
+        return self.asset_format
+    
+    def get_asset_name(self):
+        return self.asset_name
 
     def get_asset_path(self):
         return self.asset_path
+
+    def get_parent_id(self):
+        return self.parent_id
+    
+    def get_parent_name(self):
+        return self.parent_name
+    
+    def get_parent_path(self):
+        return self.parent_path
 
     def get_video_size(self):
         return self.video_size
